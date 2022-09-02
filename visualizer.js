7368c7e03b766996971fab4b3e6cd0539f148468
APareja12 <alexis.pareja@hotmail.com>
2022-09-02 13:06:56 -0400

    adds ball class

--- visualizer.js
+++ visualizer.js
@@ -1,4 +1,21 @@
 const microphone = new Microphone();
+const canvas = document.getElementById('myCanvas');
+const ctx = canvas.getContext('2d');
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+class Ball {
+    constructor(x, y){
+        this.x = x;
+        this.y = y;
+        this.radius = 8;
+        this.color = 'white';
+        this.jumpForce = 0;
+        this.fallForce = 0.1;
+        this.isFalling = true;
+    }
+}
+
 
 function animate(){
     if (microphone.initialized){
